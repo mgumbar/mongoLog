213e380ca12afeb4dc1d678b575276d5102a1d23
mgumbar <mgumbar@gmail.com>
2018-01-21 22:12:25 +0100

    Include Mongo Db Driver & Search page

---
+++ MongoLog/Models/Log.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MongoLog.Models
+{
+    public class Log
+    {
+        public ObjectId Id { get; set; }
+        public string line { get; set; }
+        public string Name { get; set; }
+        public string host { get; set; }
+        public string logname { get; set; }
+        public string user { get; set; }
+        public string time { get; set; }
+        public string path { get; set; }
+        public string request { get; set; }
+        public string status { get; set; }
+        public string response_size { get; set; }
+        public string referrer { get; set; }
+        public string user_agent { get; set; }
+    }
+}
---
+++ MongoLog/Services/LogService.cs
@@ -0,0 +1,68 @@
+﻿using MongoDB.Driver;
+using MongoLog.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MongoLog.Services
+{
+    public class LogService
+    {
+        // -----------------------------------------------------------
+        // Singleton
+        // -----------------------------------------------------------
+
+        private static object syncRoot = new object();
+
+        private static LogService instance;
+
+        public static LogService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new LogService(ConfigurationManager.ConnectionStrings["MongoServer"].ConnectionString);
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        // ------------------------------------------------------------
+
+
+        private string connectionString;
+
+        public LogService(string inConnectionString)
+        {
+            connectionString = inConnectionString;
+
+        }
+
+        /// <summary>
+        /// Récupère les logs de la base de données
+        /// </summary>
+        /// <returns></returns>
+        public List<Log> GetLogs(string host, string time)
+        {
+            var client = new MongoClient(this.connectionString);
+            var database = client.GetDatabase("log");
+            var collection = database.GetCollection<Log>("back-end");
+            var filter = Builders<Log>.Filter.Eq("host", host);
+            var list = collection.Find(x => x.host == host).ToList();
+
+            return list;
+        }
+    }
+}
