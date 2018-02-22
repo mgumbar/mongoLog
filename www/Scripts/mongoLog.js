0aa662697af8e0fa692af9ab59d7e5fc4e47f406
mgumbar <mgumbar@gmail.com>
2018-02-22 22:44:21 +0100

    include Charts

--- www/Scripts/mongoLog.js
+++ www/Scripts/mongoLog.js
@@ -33,5 +33,76 @@
         }).end();
         $('tr').highlight($("#highlightme3").val(), 'highlightRed');
     });
+    $("div.panel-heading a.pull-right").click(function (event) {
+        $("div.collapse in").collapse('hide');
+    });
+    //.active, .success, .info, .warning, .danger
+    var trActive = $('tr.active').length;
+    var trSuccess = $('tr.success').length;
+    var trInfo = $('tr.info').length;
+    var trWarning = $('tr.warning').length;
+    var trDanger = $('tr.danger').length;
+        var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+            type: 'bar',
+    data: {
+            labels: ["Error", "Info", "Success", "Warning"],
+        datasets: [{
+            label: 'Error',
+            data: [trDanger],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+        },
+            {
+                label: 'Info',
+                data: [trInfo],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            },
+            {
+                label: 'Success',
+                data: [trSuccess],
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            },
+            {
+                label: 'Warning',
+                data: [trWarning],
+                backgroundColor: [
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }
+        ]
+    },
+    options: {
+            scales: {
+            yAxes: [{
+            ticks: {
+            beginAtZero:true
+                }
+            }]
+        }
+    }
+        });
+
 });
 
